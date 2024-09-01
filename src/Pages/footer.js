1f35c9b24a5b502b92d3512ed27e7f42076a72c2
Nidhish Jaiswal <nidhishjswl@gmail.com>
2024-09-01 11:39:02 +0530

    First Commit

---
+++ src/Pages/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer()
+{
+    return (
+        <div style={{
+            paddingTop: 5,
+            paddingDown: 30,
+            paddingRight: 30,
+            textAlign: "right"
+        }}>
+            Developed by Nidhish Jaiswal
+            
+        </div>
+    );
+}
+
+export default Footer
